ece063adee9426d8c079af973d06ed0a34f18747
ammar <ammaraptech007@gmail.com>
2024-07-22 05:29:06 +0500

    all

---
+++ js/index.js
@@ -0,0 +1,72 @@
+// typo //
+document.addEventListener("DOMContentLoaded", function() {
+  const typingText = document.getElementById("typing-text");
+  const sentences = ["Software Engineer", "Front-End Developer", "Back-End Developer"];
+  let sentenceIndex = 0;
+  let charIndex = 0;
+  let isDeleting = false;
+  let typingSpeed = 100;
+
+  function type() {
+      const currentSentence = sentences[sentenceIndex];
+      if (!isDeleting && charIndex < currentSentence.length) {
+          typingText.textContent += currentSentence.charAt(charIndex);
+          charIndex++;
+          typingSpeed = 100; // Typing speed
+      } else if (isDeleting && charIndex > 0) {
+          typingText.textContent = currentSentence.substring(0, charIndex - 1);
+          charIndex--;
+          typingSpeed = 50; // Deleting speed
+      }
+
+      if (!isDeleting && charIndex === currentSentence.length) {
+          typingSpeed = 2000; // Pause before deleting
+          isDeleting = true;
+      } else if (isDeleting && charIndex === 0) {
+          isDeleting = false;
+          sentenceIndex = (sentenceIndex + 1) % sentences.length;
+          typingSpeed = 500; // Pause before typing next sentence
+      }
+
+      setTimeout(type, typingSpeed);
+  }
+
+  type();
+});
+// typo //
+// navbar//
+$(document).ready(function () {
+  $(window).scroll(function () {
+    if ($(window).scrollTop() > 0) {
+      $("nav").css("background-color", "white");
+      $(".navbar-brand").css("color", "black");
+      $("nav").css("border-color", "white");
+      $(".navbar-toggler").css("color", "black");
+      $(".nav-link").css("color", "black");
+    }
+    else {
+      $("nav").css("background-color", "transparent");
+      $(".navbar-brand").css("color", "white");
+      $("nav").css("border-color", "#989898");
+      $(".nav-link").css("color", "white");
+      $(".navbar-toggler").css("color", "white");
+    }
+  });
+});
+// navbar//
+
+// cv //
+function down(){
+  swal("Downloaded!", "My Cv is downloaded go and Check!", "success");
+}
+// cv //
+
+// project //
+document.addEventListener("mousemove", function(){
+  let pro=document.getElementById("pro");
+  let liv=document.getElementById("live");
+  if(pro){
+    liv.style.display="active";
+  }
+})
+// project //
